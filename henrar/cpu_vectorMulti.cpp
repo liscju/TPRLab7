052373f2a1650997773da8154c218c49696ab273
Bartlomiej Kumor <aargh1@gmail.com>
2015-05-25 09:24:13 +0200

    added stub for cpu version && modified gpu version

--- henrar/cpu_vectorMulti.cpp
+++ henrar/cpu_vectorMulti.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include "helper_functions"
 
+class Matrix
+{
+public:	
+	int width;
+	int height;
+	float *elements;
+};
+
+
+void prepareMatrices()
+{
+	std::fstream A_input;
+	std::fstream B_input;
+
+	const int Width = 16;
+
+	Matrix A;
+	Matrix B;
+	Matrix C;
+
+
+	A.width = Width;
+	B.width = Width;
+	C.width = Width;
+	
+	A.height = Width;
+	B.height = Width;
+	C.height = Width;
+	
+	A.elements = new float[Width*Width];
+	B.elements = new float[Width*Width];
+	C.elements = new float[Width*Width];
+}
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	prepareMatrices();
 	return 0;
 }
